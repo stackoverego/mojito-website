d725a289bfcc7a635e9b707549ab88fdc755549f
parth <parthpatil1610@gmail.com>
2025-09-15 11:23:22 +0530

    adding new art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import  Cocktails  from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
@@ -15,7 +16,8 @@ const App = () => {
       <Hero/>
       <Cocktails/>
       <About/>
-      <div className='h-screen'></div>
+      <Art/>
+     
     </main>
   )
 }
